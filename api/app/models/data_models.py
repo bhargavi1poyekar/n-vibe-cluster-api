584e7046993e92c79d65cd0f795f2e32c04cf09a
Leo <leo@n-vibe.fr>
2024-02-09 10:39:45 +0100

    fixed bug in rssi signal validation

--- api/app/models/data_models.py
+++ api/app/models/data_models.py
@@ -3,9 +3,8 @@ from typing import Union, List
 
 
 class RSSISignals(BaseModel):
-    signal: conlist(confloat(ge=-100, le=0), min_items=10, max_items=10)
+    signal: conlist(confloat(ge=-100, le=0), min_length=10, max_length=10)
 
     @field_validator("signal")
     def check_rssi_signal(cls, v: List[Union[int, float]]):
-        # Ensure all values are floats and within the specified range
         return [float(value) if isinstance(value, int) else value for value in v]
